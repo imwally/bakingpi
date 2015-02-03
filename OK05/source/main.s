2c7c366d450bddc1947e8ce5b9022a62168db97e
Wally <wally@imwally.net>
2015-02-02 19:53:10 -0500

    add comment for pattern position check

--- OK05/source/main.s
+++ OK05/source/main.s
@@ -22,6 +22,35 @@ main:
     mov r5,#0
 
     loop$:
+        /*
+        * The binary pattern in r4 resembles the Morse code for SOS with 1
+        * representing an LED flash. Register r5 is used as a position counter
+        * to check where the bit currently is in the Morse code sequence. Each
+        * time the code loops the position counter is incremented by 1. Shifting
+        * the 1 in register r1 by the position of the sequence is used to check
+        * if there is a 1 in the pattern (r5). This is achieved by doing a
+        * bitwise AND with the position and the pattern.
+        *
+        * An example:
+        *  
+        * First loop
+        * ----------
+        * r5 = 00000000000000000000000000000001
+        * r4 = 00000000010101011101110111010101
+        * r5 AND r4 = 1
+        * r1 = 1
+        *
+        * Second loop
+        * ----------
+        * r5 = 00000000000000000000000000000010
+        * r4 = 00000000010101011101110111010101
+        * r5 AND r4 = 0
+        * r1 = 0
+        *
+        * and so on...
+        *
+        * Register r1 will now be used as an argument to SetGpio.
+        */
         mov r1,#1
         lsl r1,r5
         and r1,r4
@@ -55,6 +84,7 @@ main:
 * the number following the instruction. In this instance it will ensure the
 * address for the pattern label will be a multiple of 2. This is needed as the
 * ldr instruction will only work on addresses that are a multiple of 2.
+*
 */
 .section .data
 .align 2
